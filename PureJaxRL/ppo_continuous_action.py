00801369f82c26253deb1b099be0a02afbacd2d8
julian <julian@datenvorsprung.at>
2023-06-06 18:17:38 +0200

    ContinuousCartpole dev

--- PureJaxRL/ppo_continuous_action.py
+++ PureJaxRL/ppo_continuous_action.py
@@ -10,37 +10,41 @@ import distrax
 from gymnax.environments import environment, spaces
 from brax import envs
 from gymnax.wrappers.purerl import LogWrapper, FlattenObservationWrapper
+from continuous_cartpole_jax import ContinuousCartPoleEnv
+import gymnasium
+
 
 class BraxGymnaxWrapper:
     def __init__(self, env_name, backend="positional"):
-        env = envs.get_environment(env_name=env_name, backend=backend)
-        env = envs.wrapper.EpisodeWrapper(env, episode_length=1000, action_repeat=1)
-        env = envs.wrapper.AutoResetWrapper(env)
+        env = ContinuousCartPoleEnv()
         self._env = env
-        self.action_size = env.action_size
-        self.observation_size = (env.observation_size,)
-    
+        self.action_size = env.action_space.shape
+        self.observation_size = env.observation_space.shape
+
     def reset(self, key, params=None):
         state = self._env.reset(key)
         return state.obs, state
-    
+
     def step(self, key, state, action, params=None):
         next_state = self._env.step(state, action)
         return next_state.obs, next_state, next_state.reward, next_state.done > 0.5, {}
-    
-    def observation_space(self, params):
+
+    @property
+    def observation_space(self):
         return spaces.Box(
             low=-jnp.inf,
             high=jnp.inf,
-            shape=(self._env.observation_size,),
+            shape=self.observation_size,
         )
-    
-    def action_space(self, params):
+
+    @property
+    def action_space(self):
         return spaces.Box(
             low=-1.0,
             high=1.0,
-            shape=(self._env.action_size,),
+            shape=self.action_size,
         )
+
 
 class ActorCritic(nn.Module):
     action_dim: Sequence[int]
@@ -90,6 +94,7 @@ class Transition(NamedTuple):
     obs: jnp.ndarray
     info: jnp.ndarray
 
+
 def make_train(config):
     config["NUM_UPDATES"] = (
         config["TOTAL_TIMESTEPS"] // config["NUM_STEPS"] // config["NUM_ENVS"]
@@ -98,6 +103,8 @@ def make_train(config):
         config["NUM_ENVS"] * config["NUM_STEPS"] // config["NUM_MINIBATCHES"]
     )
     env, env_params = BraxGymnaxWrapper(config["ENV_NAME"]), None
+    env = envs.wrapper.EpisodeWrapper(env, episode_length=1000, action_repeat=1)
+    env = envs.wrapper.AutoResetWrapper(env)
     env = LogWrapper(env)
 
     def linear_schedule(count):
@@ -107,9 +114,9 @@ def make_train(config):
     def train(rng):
 
         # INIT NETWORK
-        network = ActorCritic(env.action_space(env_params).shape[0], activation=config["ACTIVATION"])
+        network = ActorCritic(env.action_space.shape[0], activation=config["ACTIVATION"])
         rng, _rng = jax.random.split(rng)
-        init_x = jnp.zeros(env.observation_space(env_params).shape)
+        init_x = jnp.zeros(env.observation_space.shape)
         network_params = network.init(_rng, init_x)
         if config["ANNEAL_LR"]:
             tx = optax.chain(
@@ -127,7 +134,7 @@ def make_train(config):
         # INIT ENV
         rng, _rng = jax.random.split(rng)
         reset_rng = jax.random.split(_rng, config["NUM_ENVS"])
-        obsv, env_state = jax.vmap(env.reset, in_axes=(0, None))(reset_rng, env_params)
+        obsv, env_state = jax.vmap(env.reset)(reset_rng)
 
         # TRAIN LOOP
         def _update_step(runner_state, unused):
@@ -144,7 +151,7 @@ def make_train(config):
                 # STEP ENV
                 rng, _rng = jax.random.split(rng)
                 rng_step = jax.random.split(_rng, config["NUM_ENVS"])
-                obsv, env_state, reward, done, info = jax.vmap(env.step, in_axes=(0,0,0,None))(
+                obsv, env_state, reward, done, info = jax.vmap(env.step, in_axes=(0, 0, 0, None))(
                     rng_step, env_state, action, env_params
                 )
                 transition = Transition(
@@ -299,7 +306,7 @@ if __name__ == "__main__":
         "VF_COEF": 0.5,
         "MAX_GRAD_NORM": 0.5,
         "ACTIVATION": "tanh",
-        "ENV_NAME": "hopper",
+        "ENV_NAME": "ContinuousCartPole-v1",
         "ANNEAL_LR": True,
     }
     rng = jax.random.PRNGKey(30)
